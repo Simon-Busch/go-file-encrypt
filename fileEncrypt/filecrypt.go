3d47ce3b98b43a778c7d5301a64cbfca3ded5518
Simon Busch <Simon.busch@live.fr>
2024-07-16 17:37:11 +0200

    wip

---
+++ fileEncrypt/filecrypt.go
@@ -0,0 +1,9 @@
+package filecrypt
+
+func Encrypt(file string, password []byte) {
+	// Encrypt the file
+}
+
+func Decrypt(file string, password []byte) {
+	// Decrypt the file
+}
--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"os"
+	"golang.org/x/term"
+	"github.com/Simon-Busch/go-file-encrypt/fileEncrypt"
 )
 
 func main() {
@@ -31,7 +34,7 @@ func printHelp() {
 	fmt.Println("Usage: ")
 	fmt.Println("  go run main.go <command> [args]")
 	fmt.Println("Commands: ")
-	fmt.Println("  encrypt: Encrypt a file")
+	fmt.Println("  encrypt: Encrypt a file /path/to/file")
 	fmt.Println("  decrypt: Decrypt a file")
 	fmt.Println("  help: Print this help")
 }
@@ -40,22 +43,70 @@ func encryptHandle() {
 	fmt.Println("Encrypting...")
 	if len(os.Args) < 3 {
 		fmt.Println("Please provide a file to encrypt, missing the path")
-		os.Exit(1)
+		os.Exit(0)
 	}
+
+	file := os.Args[2]
+	if (!validateFile(file)) {
+		fmt.Println("File is valid")
+	} else {
+		fmt.Println("File is invalid")
+	}
+
+	password := getPassword()
+	fmt.Println("\n Encrypting file: ", file)
+	filecrypt.Encrypt(file, password)
+	fmt.Println("File encrypted")
 }
 
 func decryptHandle() {
 	fmt.Println("Decrypting...")
+	if len(os.Args) < 3 {
+		fmt.Println("Please provide a file to encrypt, missing the path")
+		os.Exit(0)
+	}
+
+	file := os.Args[2]
+	if (!validateFile(file)) {
+		fmt.Println("File is valid")
+	} else {
+		fmt.Println("File is invalid")
+	}
+
+	fmt.Println("Enter password:")
+	password, _ := term.ReadPassword(0)
+	fmt.Println("\n Decrypting file: ", file)
+	filecrypt.Decrypt(file, password)
+	fmt.Println("File decrypted successfuly")
 }
 
-func getPassword() {
+func getPassword() []byte {
 	fmt.Println("Enter password: ")
+	password, _ := term.ReadPassword(0)
+
+	fmt.Println("\n Confirm password: ")
+
+	confirmPassword, _ := term.ReadPassword(0)
+
+	if !validatePassword(password, confirmPassword) {
+		fmt.Println("Passwords do not match")
+		fmt.Println("Please try again")
+		getPassword()
+	}
+	return password
 }
 
-func validatePassword() {
-
+func validatePassword(password1 []byte, password2 []byte) bool {
+	if !bytes.Equal(password1, password2) {
+		return false
+	}
+	return true
 }
 
-func validateFile() {
-
+// Check if file exist
+func validateFile(file string) bool {
+	if _, err := os.Stat(file); os.IsNotExist(err) {
+		return false
+	}
+	return true;
 }
